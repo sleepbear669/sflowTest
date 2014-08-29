1be1c4919155cb54c57a597b38e7204d5e9c78c6
feverneva <feverneva.code@gmail.com>
2014-08-29 11:33:07 +0900

    add curl ifindex

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?
-	$result = "tesst";
-	echo $result;
-	$result = exec("curl http://117.17.102.118:8008/flowkeys/json");
-	print_r($result);
-	echo "abc";
+	$flowKeys;
+	$result = exec("curl http://117.17.102.118:8008/flowkeys/json", $flowKeys);
+	print_r($flowKeys);
+	$ifIndex;
+	print("\r\n\r\n")l;
+	exec("curl http://117.17.102.118/test.php", $ifIndex);
+	print_r($ifIndex);
 ?>
