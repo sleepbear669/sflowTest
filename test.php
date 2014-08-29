a1f8943389db9d4a9b4358877b53bd2288e1388e
root <root@node2.(none)>
2014-08-29 13:02:29 +0900

    change

--- test.php
+++ test.php
@@ -1,12 +1,10 @@
 <?
 	include_once "HttpRequest.php";
-	echo "123123123";
 	$flowKeys;
 	exec("curl http://117.17.102.118:8008/flowkeys/json", $flowKeys);
-	echo "123";
 	print_r($flowKeys);
 	$ifIndex;
-	echo "test1<br>";
+	echo "\r\b\r\b\est1<br>";
 	exec("curl http://117.17.102.118/test.php", $ifIndex);
 	print_r($ifIndex);
 	echo "why";
